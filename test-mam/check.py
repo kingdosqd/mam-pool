5ab6a9e9a2f569a00a7ec0260c2a2b0033f1531f
kingdosqd <kingdosqd@gmail.com>
2021-05-24 17:10:16 +0800

    检查金额数据

--- test-mam/check.py
+++ test-mam/check.py
@@ -6,18 +6,26 @@
 '''
 检查项:
 1:pow奖励金额的正确性
+2:检查发放奖励，投票金额，资金池金额
 '''
 
+import decimal
 from sqlalchemy.dialects.mysql import INTEGER, VARCHAR,DECIMAL
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from decimal import Decimal,getcontext
-import sqlalchemy
 
+from sqlalchemy.sql.expression import null, true
+from operator import itemgetter, attrgetter
+
+context = decimal.getcontext()
+context.rounding = decimal.ROUND_DOWN
 
 Base = declarative_base()
+
+engine = create_engine("mysql+pymysql://root:root@localhost:3306/minemon", encoding="utf-8")
 
 class Tx(Base):
     __tablename__ = 'tx'
@@ -31,20 +39,48 @@ class Tx(Base):
     txfee = Column(DECIMAL, nullable=False)
 
 def TestWork():
-    engine = create_engine("mysql+pymysql://root:root@localhost:3306/minemon", encoding="utf-8")
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     Txs = session.query(Tx).filter(Tx.type == 'work').all()
     height = 1
     for tx in Txs:
-        if tx.amount != Decimal('38.2') or height != tx.height:
-        #if height != tx.height:
-            print("TestWork err:",tx.height,tx.amount,tx.txid)
-            exit()
+        assert tx.amount == Decimal('38.2'),'TestWork amount(%f) err.height:%d' % (tx.amount,tx.height)
+        assert height == tx.height,'TestWork height(%d) err.' % tx.height
         height = height + 1
     session.close()
     print("TestWork OK")
 
+def TestAmount():
+    BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT = 2
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    Txs = session.query(Tx).all()
+    N_ = Decimal("100")
+    M_ = N_ * 100
+    TotalReward = Decimal("0")
+    MoneySupply = Decimal("0")
+    Vote = Decimal("0")
+    Stakes = []
+    for tx in Txs:
+        if tx.type == 'work':
+            TotalReward = TotalReward + Decimal("100.0")
+            MoneySupply = MoneySupply + Decimal("38.2")
+        if tx.type == 'stake':
+            Stake = round((TotalReward - MoneySupply) * Vote / M_,6)
+            assert tx.amount == Stake,"奖励金额不对.tx.amount:%f, Stake:%f, height:%d" % (tx.amount, Stake,tx.height)
+            Stakes.append(Stake)
+            MoneySupply = MoneySupply + Stake
+        if tx.type == 'distribute':
+            Vote = Vote + tx.amount
+            if len(Stakes) == BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT + 1:
+                assert tx.amount == sum(Stakes[:-1]),"系统发的奖励错误"
+            Stakes = Stakes[-1:]
+        if tx.sendto[0:4] == "21c0" and tx.type != "distribute":
+            assert tx.amount >= Decimal("100"),"投票金额小于100（%f" % tx.amount
+            Vote = Vote + tx.amount
+    session.close()
+    print("TestAmount OK")
+
 if __name__ == '__main__':
-    print(sqlalchemy.__version__)
-    TestWork()
+    #TestWork()
+    TestAmount()
