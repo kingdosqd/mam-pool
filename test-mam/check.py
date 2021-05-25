2e857cdcf651868b2e2f7d8c2da9e445cb66ffee
kingdosqd <kingdosqd@gmail.com>
2021-05-25 19:02:40 +0800

    投票支持多用户

--- test-mam/check.py
+++ test-mam/check.py
@@ -58,7 +58,7 @@ def TestWork():
 
 
 def TestAmount():
-    BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT = 2
+    BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT = 5
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     Txs = session.query(Tx).all()
@@ -96,7 +96,72 @@ def TestAmount():
     session.close()
     print("TestAmount OK")
 
+def TestAmount1():
+    BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT = 5
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    Txs = session.query(Tx).all()
+    info = {}
+    for tx in Txs:
+        if tx.height not in info:
+            info[tx.height] = {
+                "work":None,
+                "stake":None,
+                "distribute":[],
+                "vote":[]}
+        if tx.type == 'work':
+            info[tx.height]["work"] = tx
+        if tx.type == 'stake':
+            info[tx.height]["stake"] = tx
+        if tx.type == 'distribute':
+            info[tx.height]["distribute"].append(tx)
+        if tx.type == 'token' and tx.sendto[0:4] == "21c0":
+            info[tx.height]["vote"].append(tx)
+
+
+    N_ = Decimal("100")
+    M_ = N_ * 100
+    TotalReward = Decimal("0")
+    MoneySupply = Decimal("0")
+    Vote = {}
+    Stakes = []
+    for obj in info:
+        TotalReward = TotalReward + Decimal("100.0")
+        MoneySupply = MoneySupply + Decimal("38.2")
+        Stake = 0
+
+        if info[obj]["work"].sendto in Vote:
+            Stake = round((TotalReward - MoneySupply) * Vote[info[obj]["work"].sendto] / M_,6)
+            Stakes.append(Stake)
+            MoneySupply = MoneySupply + Stake
+        assert Stake == info[obj]["stake"].amount,"stake 金额错误"
+
+        if len(Stakes) == BBCP_PLEDGE_REWARD_DISTRIBUTE_HEIGHT + 1:
+            
+            sum_ = Decimal("0")
+            for d in info[obj]["distribute"]:
+                sum_ += d.amount
+            for d in info[obj]["distribute"]:
+                print(info[obj]["work"].sendto,d.amount / sum_)
+            print("--------------------")
+            assert sum_ == sum(Stakes[:-1]),"奖励发放错误"
+            Stakes = Stakes[-1:]
+
+        for tx in info[obj]["distribute"]:
+            if tx.pool_in in Vote:
+                Vote[tx.pool_in] += tx.amount
+            else:
+                Vote[tx.pool_in] = tx.amount
+
+        for tx in info[obj]["vote"]:
+            if tx.pool_in in Vote:
+                Vote[tx.pool_in] += tx.amount
+            else:
+                Vote[tx.pool_in] = tx.amount
+
+    session.close()
+    print("TestAmount OK")
 
 if __name__ == '__main__':
-    TestWork()
-    TestAmount()
+    #TestWork()
+    TestAmount1()
