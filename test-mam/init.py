5c21678e90bcc010f86c5b4313172781d6b53e7a
kingdosqd <84276710+kingdosqd@users.noreply.github.com>
2021-05-29 10:31:09 +0800

    Code test (#1)
    * 检查pow出块奖励
    
    * 检查金额数据
    
    * 修复pow数据检查问题-缺少交易费检查
    
    * 添加投票地址
    
    * 投票支持多用户
    
    * 节点启动增加-debug
    
    * 检查奖励的详细信息
    
    * pool addr 管理
    
    * 新版本的检查程序

--- test-mam/init.py
+++ test-mam/init.py
@@ -1,45 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#sha256address=1j6x8vdkkbnxe8qwjggfan9c8m8zhmez7gm3pznsqxgch3eyrwxby8eda
-#sha256key=28efbfda61b473c37549d02784648d89fe21ff082b7a42da9ef97b0b83cdb1a9
-
 import subprocess
 import json
 import time
+import sys
+
+privkey = "24d0ad81e6af0adc350b0f5962596d409a320a96cd7b6c2ce5dad2e65568a39a"
+pubkey = "e89fc4e7d08723124a8037a50b2d9a5006c3f54bc98261b9701bdad81d9cf341"
+addr = "187ssr7erv8dq1eb1gb4mqxe30s89mb8bmmvr0jgj4e3x1sy4kzm27jpv"
+
+if len(sys.argv) > 1:
+    privkey = "d76a62a1513a6f0f7a196536bc3916f6480c092ff458b09a0473eebae7c88f61"
+    pubkey = "d51e71c180bf2731413a0bb737cb0a73eca7d08687047fe7d5cab042b0e28f6d"
+    addr = "1dp7y5c22p35dbsvz0j3rdm57xhsgnjsqpw5kmg9h4yzr1gbh3vamb18j"
+    print("另外一台机器")
 
 def run_cmd(cmd):
     print(cmd)
-    #info = subprocess.getoutput(cmd)
-    #print(info)
-    
     info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
     print(info.stdout)
     time.sleep(1)
 
 run_cmd('minemon-cli stop')
 run_cmd('rm -rf ~/.minemon/*')
-run_cmd('cp ./minemon.conf ~/.minemon/')
-#subprocess.run(['minemon', '-daemon'])
+
+cmd = '''echo spentaddress=%s '
+pledgefee=50
+listen4' >> ~/.minemon/minemon.conf
+''' % addr
+
+if len(sys.argv) > 1:
+    cmd = '''echo spentaddress=%s '
+pledgefee=50
+connect=192.168.0.103
+listen4' >> ~/.minemon/minemon.conf
+''' % addr
+
+run_cmd(cmd)
+
 subprocess.run('minemon -daemon',shell=True)
 time.sleep(2)
-run_cmd('minemon-cli importprivkey 42b889a2668eda6d78682c23b5651fb76b5aac2b71caba1aa23b6b14d5ce75b7 123')
-run_cmd('minemon-cli unlockkey 57e7d8bb1119ec37d76f0785e73b1a3fa288a5aa1e84925fe47a5d73b68dba91 123 0')
+cmd = 'minemon-cli importprivkey %s 123' % privkey
+run_cmd(cmd)
+cmd = 'minemon-cli unlockkey %s 123 0' % pubkey
+run_cmd(cmd)
 
 # 添加挖矿模板
-cmd = "minemon-cli addnewtemplate mint '{\"mint\": \"883f29fb7740f8e625159cb6bcee42cf3ac460be8b9fcb839ccbfeda3744b217\", \"spent\": \"1j6x8vdkkbnxe8qwjggfan9c8m8zhmez7gm3pznsqxgch3eyrwxby8eda\"}'"
-run_cmd(cmd)
-# 20g053vhn4ygv9m8pzhevnjvtgbbqhgs66qv31ez39v9xbxvk0ynqmeyf
+cmd = "minemon-cli addnewtemplate mint '{\"spent\": \"%s\", \"pledgefee\":50}'" % addr
+info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
+miner_addr = info.stdout.strip("\n")
 
 # 添加投票模板
-cmd = "minemon-cli addnewtemplate mintpledge '{\"owner\": \"1j6x8vdkkbnxe8qwjggfan9c8m8zhmez7gm3pznsqxgch3eyrwxby8eda\", \"powmint\": \"20g053vhn4ygv9m8pzhevnjvtgbbqhgs66qv31ez39v9xbxvk0ynqmeyf\", \"rewardmode\":1}'"
-run_cmd(cmd)
-# 21c0ccarf3kpr87r2peet1p1ngsmsze8qdsmza6kmws9fz6k0v9y7fmnc
+#cmd = "minemon-cli addnewtemplate mintpledge '{\"owner\": \"%s\", \"powmint\": \"%s\", \"rewardmode\":1}'" % (privkey,miner_addr)
+#info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
+#vote_addr1 = info.stdout.strip("\n")
 
 # 赎回模板，投票模板只能向赎回模板转账
-cmd = "minemon-cli addnewtemplate mintredeem '{\"owner\": \"1j6x8vdkkbnxe8qwjggfan9c8m8zhmez7gm3pznsqxgch3eyrwxby8eda\", \"nonce\": 1}'"
-run_cmd(cmd)
-# 21g02epzpp8txzbtg44t6stf78djkwt2cz5w61mvsy1t00ddywwks3b68
+#cmd = "minemon-cli addnewtemplate mintredeem '{\"owner\": \"%s\", \"nonce\": 1}'" % privkey
+#info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
+#mintredeem = info.stdout.strip("\n")
+
+def Vote(miner_addr,pri):
+    cmd = "minemon-cli addnewtemplate mintpledge '{\"owner\": \"%s\", \"powmint\": \"%s\", \"rewardmode\":1}'" % (pri,miner_addr)
+    info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
+    vote_addr1 = info.stdout.strip("\n")
+    print(vote_addr1,"-->>",miner_addr)
+    cmd = "minemon-cli sendfrom %s %s 100" % (miner_addr,vote_addr1)
+    subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
 
 while True:
     time.sleep(3)
@@ -47,17 +75,51 @@ while True:
     objs = json.loads(json_str)
     is_break = False
     for obj in objs:
-        if obj["address"] == "20g053vhn4ygv9m8pzhevnjvtgbbqhgs66qv31ez39v9xbxvk0ynqmeyf" and obj["avail"]  >= 100:
+        if obj["address"] == miner_addr and obj["avail"]  > 100:
             is_break = True
             break
 
     # 给挖矿地址投票
     if is_break:
-        cmd = "minemon-cli sendfrom 20g053vhn4ygv9m8pzhevnjvtgbbqhgs66qv31ez39v9xbxvk0ynqmeyf 21c0ccarf3kpr87r2peet1p1ngsmsze8qdsmza6kmws9fz6k0v9y7fmnc 100"
-        run_cmd(cmd)
+        Vote(miner_addr,"16rqwvjcr6r458va8cfwyyc5gqh2vj0p6b1b1zan1ga1zg5vqpy3v15m1")
+        #Vote(miner_addr,"17nhq5fn5v60x05n6p446ne39rt2g2me0ffez12926st4d3kxr8mbfape")
+        #Vote(miner_addr,"1h9pd97xa2xm1w5nj2m7baft082xdy75ccj624g6v5rgc7xbnmf3c6t45")
         break
     run_cmd("minemon-cli getforkheight")
-            
+
+
 while True:
-    run_cmd('minemon-cli getbalance')
+    time.sleep(3)
+    json_str = subprocess.getoutput('minemon-cli getbalance')
+    objs = json.loads(json_str)
+    is_break = False
+    for obj in objs:
+        if obj["address"] == miner_addr and obj["avail"]  > 100:
+            is_break = True
+            break
+
+    # 给挖矿地址投票
+    if is_break:
+        #Vote(miner_addr,"16rqwvjcr6r458va8cfwyyc5gqh2vj0p6b1b1zan1ga1zg5vqpy3v15m1")
+        Vote(miner_addr,"17nhq5fn5v60x05n6p446ne39rt2g2me0ffez12926st4d3kxr8mbfape")
+        #Vote(miner_addr,"1h9pd97xa2xm1w5nj2m7baft082xdy75ccj624g6v5rgc7xbnmf3c6t45")
+        break
+    run_cmd("minemon-cli getforkheight")
+
+while True:
+    time.sleep(3)
+    json_str = subprocess.getoutput('minemon-cli getbalance')
+    objs = json.loads(json_str)
+    is_break = False
+    for obj in objs:
+        if obj["address"] == miner_addr and obj["avail"]  > 100:
+            is_break = True
+            break
+
+    # 给挖矿地址投票
+    if is_break:
+        #Vote(miner_addr,"16rqwvjcr6r458va8cfwyyc5gqh2vj0p6b1b1zan1ga1zg5vqpy3v15m1")
+        #Vote(miner_addr,"17nhq5fn5v60x05n6p446ne39rt2g2me0ffez12926st4d3kxr8mbfape")
+        Vote(miner_addr,"1h9pd97xa2xm1w5nj2m7baft082xdy75ccj624g6v5rgc7xbnmf3c6t45")
+        break
     run_cmd("minemon-cli getforkheight")
--- test-mam/sync.py
+++ test-mam/sync.py
@@ -5,6 +5,7 @@ import subprocess
 import json
 import time
 import pymysql
+import bbc_lib
 
 
 connection = pymysql.connect(host="127.0.0.1", port=3306, user="root", password="root", db="minemon")
@@ -16,15 +17,23 @@ def ExecSql(sql):
         connection.commit()
         return cursor.lastrowid
     except Exception as e:
+        print(e,sql)
         return 0
 
 
-def get_tx_data(txid,height):
+def get_tx_data(txid,height,bits):
     cmd = 'minemon-cli gettransaction %s' % txid
     info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
     objs = json.loads(info.stdout)
     tx = objs["transaction"]
-    sql = "insert tx(txid,height,type,sendfrom,sendto,amount,txfee)values('%s',%d,'%s','%s','%s',%f,%f)" % (tx["txid"],height,tx["type"],tx["sendfrom"],tx["sendto"],tx["amount"],tx["txfee"])
+    pool_in = ""
+    miner_in = ""
+    if tx["sendto"][:4] == "21c0" and tx["type"] == 'token':
+        miner_in = bbc_lib.Hex2Addr(tx["sig"][:66])
+        pool_in = bbc_lib.Hex2Addr(tx["sig"][66:66+66])
+    
+    sql = "insert tx(txid,height,type,sendfrom,sendto,amount,txfee,bits,pool_in,miner_in)values('%s',%d,'%s','%s','%s',%f,%f,'%s','%s','%s')" \
+        % (tx["txid"],height,tx["type"],tx["sendfrom"],tx["sendto"],tx["amount"],tx["txfee"],bits,pool_in,miner_in)
     #print(sql)
     ExecSql(sql)
 
@@ -37,14 +46,29 @@ def get_block_data(height):
     cmd = 'minemon-cli getblock %s' % objs[0]
     info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
     objs = json.loads(info.stdout)
-    get_tx_data(objs["txmint"],height)
+    get_tx_data(objs["txmint"],height,objs["bits"])
     for txid in objs["tx"]:
-        get_tx_data(txid,height)
+        get_tx_data(txid,height,objs["bits"])
 
-if __name__ == '__main__':
+def Run():
     cmd = 'minemon-cli getforkheight'
     info = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE,universal_newlines=True)
     height = int(info.stdout)
-    for h in range(1,height):
+    for h in range(1,height+1):
         #print(h)
         get_block_data(h)
+
+    ### 更新数据
+    sql = "select sendto,pool_in,miner_in from tx where type = 'token'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    for tr in res:
+        if tr[0][:4] == '21c0':
+            sql = "update tx set pool_in = %s,miner_in = %s where sendto = %s and pool_in = ''"
+            cursor.execute(sql,[tr[1],tr[2], tr[0]])
+    connection.commit()
+
+if __name__ == '__main__':
+    ExecSql("delete from tx;")
+    Run()
